a8232bcc0a994c1596193c4ce94f86c68921d931
deadghost99 <deadghost00.the.meinnodansei@gmail.com>
2015-09-19 03:53:52 +0900

    complete api route: -post -get -get by id -put or update -delete

--- routes/apis/api-item-01.js
+++ routes/apis/api-item-01.js
@@ -8,10 +8,27 @@ router.route('/item')
         console.log('i got post new Item request');
         res.send({message:"create a new item in the database"});
 	})
+
 	//gets all item
 	.get(function(req, res){
 		console.log('i got get list all item request');
         res.send({message:"get all item in the database"});
 	});
 
+router.route('/item/:id')
+    //get item by id
+    .get(function(req, res){
+        res.send({message:"get item by id: "+ req.param.id +" from databases"});
+    })
+
+    //update item
+    .put(function(req,res){
+        res.send({message:"change data item by id: "+ req.param.id +" at databases"});
+    })
+
+    //delete item
+    .delete(function(req,res){
+		return res.send({message:'delete an existing item by using param id: ' + req.param.id})
+	});
+    
 module.exports = router;
