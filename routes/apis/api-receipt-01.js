870c419a2c8c3c599689a42274e2e757af772cb8
deadghost99 <deadghost00.the.meinnodansei@gmail.com>
2015-09-19 15:37:37 +0900

    complete http request new recipt, get all recipt, rev recipt, and get recipt by id

--- routes/apis/api-receipt-01.js
+++ routes/apis/api-receipt-01.js
@@ -2,33 +2,25 @@
 var express = require('express');
 var router = express.Router();
 
-router.route('/item')
+router.route('/receipt')
 	//creates a new item
 	.post(function(req, res){
-        console.log('i got post new Item request');
-        res.send({message:"create a new item in the database"});
+        res.send({message:"create a new receipt from bag"});
 	})
 
 	//gets all item
 	.get(function(req, res){
-		console.log('i got get list all item request');
-        res.send({message:"get all item in the database"});
+        res.send({message:"get all receipts"});
 	});
 
-router.route('/item/:id')
+router.route('/receipt/:id')
     //get item by id
     .get(function(req, res){
-        res.send({message:"get item by id: "+ req.params.id +" from databases"});
+        res.send({message:"get recipt by id: "+ req.params.id +" from databases"});
     })
 
     //update item
-    .put(function(req,res){
+    .post(function(req,res){
         res.send({message:"change data item by id: "+ req.params.id +" at databases"});
-    })
-
-    //delete item
-    .delete(function(req,res){
-		return res.send({message:'delete an existing item by using param id: ' + req.params.id})
-	});
-    
+    });
 module.exports = router;
