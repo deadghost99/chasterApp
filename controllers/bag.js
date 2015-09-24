0802dc655defb0456bce63d23f514dceeceef73c
deadghost99 <deadghost00.the.meinnodansei@gmail.com>
2015-09-24 14:48:07 +0800

    create bag controller

--- controllers/bag.js
+++ controllers/bag.js
@@ -8,3 +8,19 @@
    * reset - Delete all Item in Bag 
    * delete - Delete bag
 */
+
+var Bag = require('../models/bag.js');
+var BagItem = require('../models/bag-item.js');
+
+
+exports.create = function(req, res) {
+    
+    if(!req.body.totalPrice) req.body.totalPrice = 0;
+    
+    bag = new Bag({totalprice: req.body.totalPrice});
+    bag.save(function(err, bag) {
+        if (err) return handleError(500, err);
+        
+        return res.json(bag);
+    });
+};
