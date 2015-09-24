6c8a058234010603dff5f0d1a8e546574dfaa788
deadghost99 <deadghost00.the.meinnodansei@gmail.com>
2015-09-24 14:54:23 +0800

    init functional controller

---
+++ controllers/bag-item.js
@@ -0,0 +1,10 @@
+/* The item controller
+   Exports 3 methods:
+   * put - put new Item in Bag By Bag Id and Item Id and qty
+   * list - Get list Items data by bag Id
+   * update - change qty Item
+   * take - Delete Item in Bag
+   + clean - Delete all Item in Bag where qty = 0
+   * reset - Delete all Item in Bag
+*/
+
