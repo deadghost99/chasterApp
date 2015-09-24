38ec1e3972c9d52e450e998693af4e1f524ec682
deadghost99 <deadghost00.the.meinnodansei@gmail.com>
2015-09-24 14:28:42 +0800

    init dunctionality for controller

---
+++ controllers/bag.js
@@ -0,0 +1,10 @@
+/* The item controller
+   Exports 3 methods:
+   * create - Create new Bag
+   * show - Get Bag data by bag Id
+   * put - Edit Item data in Bag By Bag Id and Item Id and qty
+   * take - Delete Item in Bag
+   + clean - Delete all Item in Bag where qty = 0
+   * reset - Delete all Item in Bag 
+   * delete - Delete bag
+*/
