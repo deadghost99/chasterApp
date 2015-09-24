ba5b353421d299ce39c08e02c1a0e6afff26ddea
deadghost99 <deadghost00.the.meinnodansei@gmail.com>
2015-09-24 15:09:00 +0800

    put controller

--- controllers/bag-item.js
+++ controllers/bag-item.js
@@ -8,3 +8,20 @@
    * reset - Delete all Item in Bag
 */
 
+var BagItem = require('../models/bag-item.js');
+
+exports.put = function(req, res) {  
+    bagItem = new BagItem({bag_id: req.body.bag_id, 
+                           item_id: req.body.item_id, 
+                           name: req.body.name,
+                           about: req.body.about,
+                           price: req.body.price,
+                           qty: req.body.qty,
+                           total: (req.body.price * req.body.qty)
+                          });
+    bagItem.save(function(err, bagItem) {
+        if (err) return handleError(500, err);
+        
+        return res.json(bagItem);
+    });
+};
