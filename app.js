e088453f582b45d5e45df496a088a3be8762ce6e
deadghost00 <arya.ddg00@gmail.com>
2015-11-12 12:15:45 +0900

    allow cross domain

--- app.js
+++ app.js
@@ -29,6 +29,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.all("/files/*", function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  return next();
+});
+
+app.all("/files/*", function(req, res, next) {
+  if (req.method.toLowerCase() !== "options") {
+    return next();
+  }
+  return res.send(204);
+});
+
 //app.use('/api.01', api01);
 //app.use('/api.02', api02);
 //app.use('/api.03', api03);
